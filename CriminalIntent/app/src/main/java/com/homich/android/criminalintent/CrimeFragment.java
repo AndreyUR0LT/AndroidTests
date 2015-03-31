8e16c23d6b0a7b5d83ac657211e8186f9ecae658
AndreyUR0LT <andrey.ur0lt@gmail.com>
2015-03-31 22:44:11 +0300

    First commit!

---
+++ CriminalIntent/app/src/main/java/com/homich/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,79 @@
+package com.homich.android.criminalintent;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+
+import java.util.UUID;
+
+/**
+ * Created by root on 02.01.15.
+ */
+public class CrimeFragment extends android.support.v4.app.Fragment {
+
+    public static final String EXTRA_CRIME_ID = "com.homich.android.criminalintent.crime_id";
+
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        UUID crimeId = (UUID)getActivity().getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(mCrime.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+
+        CharSequence s = android.text.format.DateFormat.format("EEEE',' MMM d',' yyyy", mCrime.getDate());
+        mDateButton.setText(s);
+        //mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mCrime.setSolved(mSolvedCheckBox.isChecked());
+            }
+        });
+
+        return v;
+    }
+
+}
---
+++ CriminalIntent/app/src/main/java/com/homich/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.homich.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by root on 09.03.15.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
